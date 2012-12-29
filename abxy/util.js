674142be88aad82370c8d9bf1c9b8c8485dec3e7
Emily Eisenberg <xymostech@gmail.com>
2012-12-28 23:36:15 -0700

    Add random number utilities

--- abxy/util.js
+++ abxy/util.js
@@ -6,6 +6,17 @@ var util = {
     defarg: function(val, def) {
         return typeof val !== 'undefined' ? val : def;
     },
+
+    /* generates a random number between two endpoints */
+    random: function(low, high) {
+        return Math.random() * (high - low) + low;
+    },
+
+    /* generates a random integer between two endpoints, excluding the high
+     * point */
+    randint: function(low, high) {
+        return Math.floor(ABXY.util.random(low, high));
+    },
 };
 
 return util;
