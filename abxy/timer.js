ca480c26ae437d774286db509a9b7fcf15d0668a
Emily Eisenberg <xymostech@gmail.com>
2012-12-29 20:56:11 -0700

    Add a timer utility
    The timer attempts to use the highest-resolution timer available.

---
+++ abxy/timer.js
@@ -0,0 +1,19 @@
+/* timer module
+ * uses the highest accuracy timer available */
+ABXY.timer = (function() {
+
+var timer = {};
+
+timer.slow = Date.now;
+
+if (window.performance.now) {
+    timer.now = function() { return window.performance.now(); };
+} else if (window.performance.webkitNow) {
+    timer.now = function() { return window.performance.webkitNow(); };
+} else {
+    timer.now = Date.now;
+}
+
+return timer;
+
+})();
