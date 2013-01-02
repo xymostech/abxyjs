881958ff77fd13a376e8e31aa27cb01ae12e5800
Emily Eisenberg <xymostech@gmail.com>
2013-01-02 10:08:26 -0700

    Fix timer if window.performance doesn't exist

--- abxy/timer.js
+++ abxy/timer.js
@@ -6,9 +6,9 @@ var timer = {};
 
 timer.slow = Date.now;
 
-if (window.performance.now) {
+if (window.performance && window.performance.now) {
     timer.now = function() { return window.performance.now(); };
-} else if (window.performance.webkitNow) {
+} else if (window.performance && window.performance.webkitNow) {
     timer.now = function() { return window.performance.webkitNow(); };
 } else {
     timer.now = Date.now;
