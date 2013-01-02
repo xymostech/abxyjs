b04ec315db46ccd70717dde91b93a086e79d0eef
Emily Eisenberg <xymostech@gmail.com>
2013-01-02 09:48:07 -0700

    Add a scaling option to worldstage2d
    Add a demonstration in the example game

--- abxy/worldstage2d.js
+++ abxy/worldstage2d.js
@@ -5,12 +5,14 @@ var worldstage2d = ABXY.worldstage.Extend({
         this._super(name, options);
 
         this.position = new ABXY.vector2d(0, 0);
+        this.scale = new ABXY.vector2d(1, 1);
     },
 
     Draw: function(context) {
         context.save();
 
         context.translate(-this.position.x, -this.position.y);
+        context.scale(this.scale.x, this.scale.y);
 
         this._super(context);
 
