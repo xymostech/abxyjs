8af338f6798adf7b0c71884d913fda1fa58eeaa8
Emily Eisenberg <xymostech@gmail.com>
2013-01-03 12:09:35 -0700

    Add some more math functions

--- abxy/math.js
+++ abxy/math.js
@@ -11,6 +11,17 @@ var math = {
     randint: function(low, high) {
         return Math.floor(ABXY.math.random(low, high));
     },
+
+    /* performs a linear interpolation of the first value to the second value,
+     * with the third value as the amount of interpolation */
+    lerp: function(from, to, l) {
+        return (to - from) * l + from;
+    },
+
+    /* clamps the first value to a value between the second and third values */
+    clamp: function(val, low, high) {
+        return (val < low) ? low : (val > high) ? high : val;
+    },
 };
 
 return math;
