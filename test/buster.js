4f82bb10f97fa0b23979420b506392450d058c23
Emily Eisenberg <xymostech@gmail.com>
2013-01-12 11:27:56 -0800

    Add a test file using buster

---
+++ test/buster.js
@@ -0,0 +1,33 @@
+var config = module.exports;
+
+config["ABXY tests"] = {
+    rootPath: "../",
+    environment: "browser",
+    libs: [
+        "jquery.min.js",
+        "underscore-min.js",
+    ],
+    sources: [
+        "abxy/abxy.js",
+        "abxy/base.js",
+        "abxy/util.js",
+        "abxy/math.js",
+        "abxy/vector.js",
+        "abxy/timer.js",
+        "abxy/messagepasser.js",
+        "abxy/game.js",
+        "abxy/stage.js",
+        "abxy/worldstage.js",
+        "abxy/worldstage2d.js",
+        "abxy/world.js",
+        "abxy/entity.js",
+        "abxy/entity2d.js",
+        "abxy/sprite.js",
+        "abxy/spritesheet2d.js",
+        "abxy/key.js",
+        "abxy/sound.js",
+    ],
+    tests: [
+        "test/*-test.js"
+    ]
+}
