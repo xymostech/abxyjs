a1c6e6f6fd2f9cae0e4af8d261ffb81ce8a41535
Emily Eisenberg <xymostech@gmail.com>
2013-01-13 13:18:03 -0700

    Automate testing of all abxy modules

--- test/lint-test.js
+++ test/lint-test.js
@@ -66,90 +66,15 @@ var default_options = {
 
 var default_globals = {
     "_": false,
-    "ABXY": false
+    "ABXY": true
 };
 
-buster.testCase("lint", {
-    "lint abxy.js": function(done) {
-        test_lint(done, "abxy", default_options, {
-            "_": false,
-            "ABXY": true
-        });
-    },
+var tests = {};
 
-    "lint base.js": function(done) {
-        test_lint(done, "base", default_options, default_globals);
-    },
-
-    "lint collide.js": function(done) {
-        test_lint(done, "collide", default_options, default_globals);
-    },
-
-    "lint entity.js": function(done) {
-        test_lint(done, "entity", default_options, default_globals);
-    },
-
-    "lint entity2d.js": function(done) {
-        test_lint(done, "entity2d", default_options, default_globals);
-    },
-
-    "lint game.js": function(done) {
-        test_lint(done, "game", default_options, default_globals);
-    },
-
-    "lint key.js": function(done) {
-        test_lint(done, "key", default_options, default_globals);
-    },
-
-    "lint math.js": function(done) {
-        test_lint(done, "math", default_options, default_globals);
-    },
-
-    "lint messagepasser.js": function(done) {
-        test_lint(done, "messagepasser", default_options, default_globals);
-    },
-
-    "lint mouse.js": function(done) {
-        test_lint(done, "mouse", default_options, default_globals);
-    },
-
-    "lint sound.js": function(done) {
-        test_lint(done, "sound", default_options, default_globals);
-    },
-
-    "lint sprite.js": function(done) {
-        test_lint(done, "sprite", default_options, default_globals);
-    },
-
-    "lint spritesheet2d.js": function(done) {
-        test_lint(done, "spritesheet2d", default_options, default_globals);
-    },
-
-    "lint stage.js": function(done) {
-        test_lint(done, "stage", default_options, default_globals);
-    },
-
-    "lint timer.js": function(done) {
-        test_lint(done, "timer", default_options, default_globals);
-    },
-
-    "lint util.js": function(done) {
-        test_lint(done, "util", default_options, default_globals);
-    },
-
-    "lint vector.js": function(done) {
-        test_lint(done, "vector", default_options, default_globals);
-    },
-
-    "lint world.js": function(done) {
-        test_lint(done, "world", default_options, default_globals);
-    },
-
-    "lint worldstage.js": function(done) {
-        test_lint(done, "worldstage", default_options, default_globals);
-    },
-
-    "lint worldstage2d.js": function(done) {
-        test_lint(done, "worldstage2d", default_options, default_globals);
-    },
+_.each(ABXY._main_dependencies, function(v, k) {
+    tests["lint " + k + ".js"] = function(done) {
+        test_lint(done, k, default_options, default_globals);
+    };
 });
+
+buster.testCase("lint", tests);
